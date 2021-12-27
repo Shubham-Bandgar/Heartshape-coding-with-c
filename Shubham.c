abb8418eeb5c9c2976502a570a9a84adf17f99b5
Shubham <95328914+Shubham-Bandgar@users.noreply.github.com>
2021-12-27 19:02:31 +0530

    Add files via upload

---
+++ Shubham.c
@@ -0,0 +1,84 @@
+//FAB CODING
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    int i, j, n;
+    char name[50];
+    int len;
+    printf("WELCOME IN THE FAB CODING");
+
+    printf("\nEnter your best friend name: ");
+    gets(name);
+ 
+    printf("Enter value of n : ");
+    scanf("%d", &n);
+
+    len = strlen(name);
+
+    
+    for(i=n/2; i<=n; i+=2)
+    {
+        for(j=1; j<n-i; j+=2)
+        {
+            printf(" ");
+        }
+ 
+        for(j=1; j<=i; j++)
+        {
+            printf("♦️");
+        }
+ 
+        for(j=1; j<=n-i; j++)
+        {
+            printf(" ");
+        }
+ 
+        for(j=1; j<=i; j++)
+        {
+            printf("♦️");
+        }
+ 
+        printf("\n");
+    }
+ 
+    
+    for(i=n; i>=1; i--)
+    {
+        for(j=i; j<n; j++)
+        {
+            printf(" ");
+        }
+        
+       
+        if(i == n) 
+        {
+            for(j=1; j<=(n * 2-len)/2; j++)
+            {
+                printf("♦️");
+            }   
+
+            printf("%s", name);
+
+            for(j=1; j<(n*2-len)/2; j++)
+            {
+                printf("♦️");
+            }
+        }
+        else 
+        {
+            for(j=1; j<=(i*2)-1; j++)
+            {
+                printf("♦️");
+            }
+        }
+ 
+        printf("\n");
+        
+    }
+ printf("\n       FRIENDSHIP FOREVER ❤️🤝🤝");
+    return 0;
+}
+    
